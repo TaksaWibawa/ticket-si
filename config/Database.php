9dbeef06fb3820029657d841796b68a6e4946ed5
Faisal Afandi <faisalafandi12345@gmail.com>
2023-07-05 02:58:30 +0800

    modified:   admin/dashboard/index.php 	modified:   admin/paket/index.php 	modified:   admin/tiket/index.php 	modified:   admin/verif_paket/index.php 	modified:   admin/verif_tiket/index.php 	modified:   config/Database.php 	modified:   general/dashboard/index.php 	modified:   general/paket/beli.php 	modified:   general/paket/index.php 	admin/laporan/

--- config/Database.php
+++ config/Database.php
@@ -23,3 +23,29 @@ function alert($location, $title, $message, $icon)
 }
 
 define('SITE_URL', 'http://localhost/kuliah/si/git/ticket-si'); //ini sesuaiin sama punya kita
+
+// <?php
+
+// date_default_timezone_set('Asia/Ujung_Pandang');
+
+// $host = "localhost:3310"; 
+// $user = "root";
+// $pass = "";
+// $nama_db = "si_tiket"; //nama database
+// $conn = new mysqli($host, $user, $pass, $nama_db);
+
+// if (!$conn) { //jika tidak terkoneksi maka akan tampil error
+//   die("Koneksi database gagal: " . mysqli_connect_error());
+// }
+
+// function alert($location, $title, $message, $icon)
+// {
+//   echo "
+// 			<script>
+// 				alert('$message');
+// 				location.href = '$location';
+// 			</script>
+// 		";
+// }
+
+// define('SITE_URL', 'http://localhost:3000'); //ini sesuaiin sama punya kita
